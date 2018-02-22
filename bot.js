90b4baa75717321687329d532e95b7dcf9fc8f74
Byakuya <36718024+spike12209@users.noreply.github.com>
2018-02-21 22:02:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'i love you') {
+    	message.reply('i love you too <3');
   	}
 });
+
+client.on('message', message => {
+    if (message.content === 'ali') {
+    	message.reply('NOO SHES MINE ILL KILL YOU IF YOU TOUCH HER');
+  	}
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
